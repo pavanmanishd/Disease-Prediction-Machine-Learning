8e6fb69455e2e5442964a3975a5ae807d727d2ca
saii <saii@gmail.com>
2023-04-03 21:25:59 +0530

    Cleaned Project Structure

--- server/main.py
+++ server/main.py
@@ -3,8 +3,8 @@ import pandas as pd
 import json
 import sys
 
-model = pickle.load(open('model.pkl','rb'))
-symps = pickle.load(open('symptoms.pkl','rb'))  
+model = pickle.load(open('./model/model.pkl','rb'))
+symps = pickle.load(open('./data/symptoms.pkl','rb'))  
 
 f = open('symptoms.txt','r')
 
@@ -21,18 +21,18 @@ sym = pd.DataFrame(s).T
 sym.columns = symps
 test_pred = model.predict(sym)
 # print(test_pred)
-desc_data = pd.read_csv('symptom_Description.csv')
+desc_data = pd.read_csv('./data/symptom_Description.csv')
 index = desc_data[desc_data["Disease"] == test_pred[0].strip()].index.values
 description = desc_data.iloc[index[0]].values
 description = description.tolist()
 
-prec_data = pd.read_csv('symptom_precaution.csv')
+prec_data = pd.read_csv('./data/symptom_precaution.csv')
 prec_index = prec_data[prec_data["Disease"] == test_pred[0].strip()].index.values
 precaution = prec_data.iloc[prec_index[0]].values
 precaution = precaution.tolist()
 disease_info = description + precaution[1:]
 
-diagnosis_data = pd.read_csv('Disease_diagnosis.csv')
+diagnosis_data = pd.read_csv('./data/Disease_diagnosis.csv')
 diagnosis_index = diagnosis_data[diagnosis_data["Disease"] == test_pred[0].strip()].index.values
 diagnosis = diagnosis_data.iloc[diagnosis_index[0]].values
 diagnosis = diagnosis.tolist()
--- server/server.js
+++ server/server.js
@@ -2,22 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const fs = require('fs');
-// const { exec } = require('child_process');
 const spawn = require('child_process').spawn;
 app.use(cors());
 app.use(express.json());
 
 app.get('/predict/:str', (req, res) => {
     console.log("Message sent to client");
-    // res.json({ message: "Hello from server!" });
     const { str } = req.params;
-    // const j = {"symptoms": str}
-    // console.log(j);
     fs.writeFile('./symptoms.txt', str, 'utf8', () => {
         console.log("File written");
-            // res.json({ message: "Hello from server!" });
-            // else{
-                const child = spawn('python3', ['./main.py']);
+                const child = spawn('py', ['./main.py']);
                 child.stdout.on('data',  (data) => {
                     const msg = data.toString().trim();
                     console.log(data.toString());
@@ -28,23 +22,9 @@ app.get('/predict/:str', (req, res) => {
                     else{
                         res.status(500).send("Error");
                     }
-                    // res.send(data.toString());
                 });    
-            // }
         
     });
-
-    
-
-    // exec(`python3 python/main.py ${str}`, (err, stdout, stderr) => {
-    //     if (err) {
-    //         console.log(err);
-    //         return;
-    //     }
-    //     console.log(stdout);
-    //     const json = require('./disease_info.json');
-    //     res.json(json);
-    // });
 });
 
 app.listen(8000, () => {
